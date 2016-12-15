bbda6093e52bab4a6bf64023c1defc21d5e00f27
Hack Reactor Students <accounts@hackreactor.com>
2016-12-15 11:04:12 -0800

    Complete linkedList to pass all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,42 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.values = {};
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail === null) {
+      list.tail = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    list.values[(newNode.value).toString()] = newNode.value;
+
   };
 
   list.removeHead = function() {
+    var currentHead = list.head.value;
+    if (list.head !== null) {
+      var newHead = list.head.next;
+      delete list.head;
+      list.head = newHead;
+    }
+    delete list.values[currentHead.toString()];
+    return currentHead;
+    
   };
 
   list.contains = function(target) {
+    var flag = false;
+    for (var item in list.values) {
+      if (+item === target) {
+        flag = true;
+      }
+    }
+    return flag;
   };
 
   return list;
