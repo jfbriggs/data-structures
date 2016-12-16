56ccaf48635b7cf542d4a84b01df994f9574b4e8
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 10:46:52 -0800

    Add complexity answers for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,18 +6,18 @@ var Set = function() {
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { // constant
   this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { // constant
   if (this._storage[item] === item) {
     return true;
   }
   return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { // constant
   delete this._storage[item];
 };
 
