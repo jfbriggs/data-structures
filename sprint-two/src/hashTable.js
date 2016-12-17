a8ffbc4b47ad8ccd1a6f451a4719150819a52608
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 11:41:56 -0800

    Add functionality for hash table resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,7 +5,7 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {  // linear
+HashTable.prototype.insert = function(k, v) {  // constant (approaching linear in worst case)
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
@@ -24,14 +24,24 @@ HashTable.prototype.insert = function(k, v) {  // linear
     this._storage.set(index, indexPairs);
   }
 
+  var count = 0;
+  this._storage.each(function(index) {
+    if (index !== undefined) {
+      count = count + index.length;
+    }
+  });
 
+
+  if (count > (this._limit * 0.75)) {
+    this.expand();
+  }
   
 };
 
-HashTable.prototype.retrieve = function(k) { // linear
+HashTable.prototype.retrieve = function(k) { // constant (approaching linear in worst case)
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  var indexPairs = this._storage.get(index); // ['Steven', 'Seagal']
+  var indexPairs = this._storage.get(index); 
   if (indexPairs === undefined) {
     return undefined;
   }
@@ -44,14 +54,68 @@ HashTable.prototype.retrieve = function(k) { // linear
 
 };
 
-HashTable.prototype.remove = function(k) { // linear
+HashTable.prototype.remove = function(k) { // constant (approaching linear in worst case)
   var index = getIndexBelowMaxForKey(k, this._limit);
   var indexPairs = this._storage.get(index);
   for (var i = 0; i < indexPairs.length; i++) {
     if (indexPairs[i][0] === k) {
-      indexPairs[i][1] = undefined;
+      indexPairs.splice(i, 1);
     }
   }
+
+  var count = 0;
+  this._storage.each(function(index) {
+    if (index !== undefined) {
+      count = count + index.length;
+    }
+  });
+
+  if (count < this._limit * 0.25) {
+    this.shrink();
+  }
+
+};
+
+HashTable.prototype.expand = function() { // linear
+  var tempStorage = [];
+  this._storage.each(function(index) {
+    if (index !== undefined) {
+      index.forEach(function(pair) {
+        tempStorage.push(pair);
+      });
+    }
+  });
+
+  this._limit = this._limit * 2;
+  this._storage = LimitedArray(this._limit);
+
+  var table = this;
+
+  tempStorage.forEach(function(pair) {
+    table.insert(pair[0], pair[1]);
+  });
+
+};
+
+HashTable.prototype.shrink = function() {  // linear
+
+  var tempStorage = [];
+  this._storage.each(function(index) {
+    if (index !== undefined) {
+      index.forEach(function(pair) {
+        tempStorage.push(pair);
+      });
+    }
+  });
+
+  this._limit = this._limit / 2;
+  this._storage = LimitedArray(this._limit);
+
+  var table = this;
+
+  tempStorage.forEach(function(pair) {
+    table.insert(pair[0], pair[1]);
+  });
 };
 
 
@@ -59,10 +123,3 @@ HashTable.prototype.remove = function(k) { // linear
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-// [[['Steven', 'Seagal'], ['John', 'Stamos']], [], [], [], [], [], [], []]
-
-// 'Steven' --> 0 --> storage[0] --> iterate: find array where [0] = 'Steven' --> return [1] from that array
-
-[[['Steven', 'Seagal']], , , , , , , ]
